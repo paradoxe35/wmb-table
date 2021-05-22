731074faf564a21853cf7cc802f3bdac5fd874d6
pngwasi <paradoxengwasi@gmail.com>
2021-05-22 11:36:13 +0200

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import App from './views/app';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Router>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
