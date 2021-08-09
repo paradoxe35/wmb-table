5ac93deae2b8ee50e3f3f7080f2a82d217d274a9
pngwasi <paradoxengwasi@gmail.com>
2021-08-09 16:18:23 +0200

    feat: useValueStateRef to use memo

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -12,9 +12,7 @@ import { rafThrottle, respondToVisibility } from './functions';
 export function useValueStateRef<T>(value: T) {
   const ref = useRef<T>(value);
 
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
+  ref.current = useMemo(() => value, [value]);
 
   return ref;
 }
