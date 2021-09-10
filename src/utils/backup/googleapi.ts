a7639047c9700dc10699866476ad79507bde1d0a
pngwasi <paradoxengwasi@gmail.com>
2021-09-10 21:20:04 +0200

    fix undifined env varibale loggedIn html

--- src/utils/backup/googleapi.ts
+++ src/utils/backup/googleapi.ts
@@ -35,8 +35,8 @@ async function authorize(
 
   // start webserver from child process
 
-  process.env.CHILD_APP_NAME = APP_NAME;
-  process.env.CHILD_WEBSITE_LINK = process.env.WEBSITE_LINK;
+  process.env.APP_NAME = APP_NAME;
+  process.env.WEBSITE_LINK = process.env.WEBSITE_LINK;
 
   const child = childProcess.fork(childsProcessesPath('loggedin.js'), {
     env: process.env,
