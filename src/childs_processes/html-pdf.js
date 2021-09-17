1e2c6a43b6431c5c2dbcc70e07db9b96a5c39684
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2021-09-18 00:53:17 +0200

    wrap html data with standard html parent

--- src/childs_processes/html-pdf.js
+++ src/childs_processes/html-pdf.js
@@ -3,18 +3,29 @@ const fs = require('fs');
 
 (async () => {
   const path = process.env.FILE_PATH;
-  const data = process.env.HTML_DATA;
+  let data = process.env.HTML_DATA;
 
   if (!path || !data || !process.send) {
     process.send && process.send(null);
     return;
   }
 
+  data = `<!DOCTYPE html><html><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>body{ font-family: sans-serif;}</style></head><body>${data}</body></html>`;
+
   try {
     pdf
       .create(data, {
         childProcessOptions: { detached: false },
         phantomPath: undefined,
+        format: 'A4',
+        border: {
+          top: '0.3in',
+          right: '0.3in',
+          bottom: '0.3in',
+          left: '0.3in',
+        },
+        quality: '99',
+        orientation: 'portrait',
       })
       .toStream(function (err, stream) {
         if (err) {
