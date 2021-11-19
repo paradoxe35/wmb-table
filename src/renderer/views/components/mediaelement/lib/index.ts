2820be9080a02832b5b3b6e51427f03b1b435ad7
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2021-11-20 00:41:30 +0200

    remove css import medialelement

--- src/renderer/views/components/mediaelement/lib/index.ts
+++ src/renderer/views/components/mediaelement/lib/index.ts
@@ -1,8 +1,3 @@
-// import './mediaelementplayer.css';
-// import './plugins/css/speed.min.css';
-// import './plugins/css/skip-back.min.css';
-// import './plugins/css/jump-forward.min.css';
-// js
 import './mediaelement-and-player.min.js';
 import './plugins/js/skip-back.min.js';
 import './plugins/js/jump-forward.min.js';
