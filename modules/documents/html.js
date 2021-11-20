5c81cf1ea52d7fd3f70a2d32b233df93928c89ae
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2021-11-21 00:20:31 +0200

    feat: document controller button with tooltip

--- modules/documents/html.js
+++ modules/documents/html.js
@@ -25,10 +25,10 @@ export const pauseIcon = /* html */ `
 `;
 
 const controllerBarHtml = /* html */ `
-  <div class="controller--bar">
-    <div class="box">
+  <div class="controller--bar clearfix">
+    <div class="box clearfix">
 
-      <div title="Recherche" tabindex="0" class="button search--open--js" role="button">
+      <div tooltip="Recherche" tabindex="0" class="button search--open--js" role="button">
           <svg viewBox="64 64 896 896" focusable="false" data-icon="file-search" width="1em" height="1em"
               fill="currentColor" aria-hidden="true">
               <path
@@ -36,7 +36,7 @@ const controllerBarHtml = /* html */ `
               </path>
           </svg>
       </div>
-      <div title="Paragraphe" tabindex="0" class="button search--paragraph--js" role="button">
+      <div tooltip="Paragraphe" tabindex="0" class="button search--paragraph--js" role="button">
         <svg viewBox="64 64 896 896" focusable="false" data-icon="sort-ascending" width="1em" height="1em" fill="currentColor" aria-hidden="true">
           <path d="M839.6 433.8L749 150.5a9.24 9.24 0 00-8.9-6.5h-77.4c-4.1 0-7.6 2.6-8.9 6.5l-91.3 283.3c-.3.9-.5 1.9-.5 2.9 0 5.1 4.2 9.3 9.3 9.3h56.4c4.2 0 7.8-2.8 9-6.8l17.5-61.6h89l17.3 61.5c1.1 4 4.8 6.8 9 6.8h61.2c1 0 1.9-.1 2.8-.4 2.4-.8 4.3-2.4 5.5-4.6 1.1-2.2 1.3-4.7.6-7.1zM663.3 325.5l32.8-116.9h6.3l32.1 116.9h-71.2zm143.5 492.9H677.2v-.4l132.6-188.9c1.1-1.6 1.7-3.4 1.7-5.4v-36.4c0-5.1-4.2-9.3-9.3-9.3h-204c-5.1 0-9.3 4.2-9.3 9.3v43c0 5.1 4.2 9.3 9.3 9.3h122.6v.4L587.7 828.9a9.35 9.35 0 00-1.7 5.4v36.4c0 5.1 4.2 9.3 9.3 9.3h211.4c5.1 0 9.3-4.2 9.3-9.3v-43a9.2 9.2 0 00-9.2-9.3zM416 702h-76V172c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v530h-76c-6.7 0-10.5 7.8-6.3 13l112 141.9a8 8 0 0012.6 0l112-141.9c4.1-5.2.4-13-6.3-13z"></path>
         </svg>
@@ -44,7 +44,7 @@ const controllerBarHtml = /* html */ `
 
       <div tabindex="-1" class="separator"></div>
 
-      <div title="Occurrence précédente" tabindex="0" class="button search--field display-none search--prev--js" role="button">
+      <div tooltip="Occurrence précédente" tabindex="0" class="button search--field display-none search--prev--js" role="button">
           <svg viewBox="64 64 896 896" focusable="false" data-icon="arrow-up" width="1em" height="1em"
               fill="currentColor" aria-hidden="true">
               <path
@@ -52,7 +52,7 @@ const controllerBarHtml = /* html */ `
               </path>
           </svg>
       </div>
-      <div title="Prochaine Occurrence" tabindex="0" class="button search--field display-none search--next--js" role="button">
+      <div tooltip="Prochaine occurrence" tabindex="0" class="button search--field display-none search--next--js" role="button">
           <svg viewBox="64 64 896 896" focusable="false" data-icon="arrow-down" width="1em" height="1em"
               fill="currentColor" aria-hidden="true">
               <path
@@ -61,7 +61,7 @@ const controllerBarHtml = /* html */ `
           </svg>
       </div>
       <div tabindex="-1" class="button search--field display-none search--result--js">0/0</div>
-      <div title="Fermer" tabindex="0" class="button search--field display-none search--close--js" role="button">
+      <div tooltip="Fermer la recherche" tabindex="0" class="button search--field display-none search--close--js" role="button">
           <svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em"
               fill="currentColor" aria-hidden="true">
               <path
@@ -72,12 +72,12 @@ const controllerBarHtml = /* html */ `
 
       <div tabindex="-1" class="separator display-none search--field"></div>
 
-      <div title="Dézoomer" tabindex="0" class="button search--zoom-out--js" role="button">
+      <div tooltip="Dézoomer" tabindex="0" class="button search--zoom-out--js" role="button">
         <svg viewBox="64 64 896 896" focusable="false" data-icon="zoom-out" width="1em" height="1em" fill="currentColor" aria-hidden="true">
           <path d="M637 443H325c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h312c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8zm284 424L775 721c122.1-148.9 113.6-369.5-26-509-148-148.1-388.4-148.1-537 0-148.1 148.6-148.1 389 0 537 139.5 139.6 360.1 148.1 509 26l146 146c3.2 2.8 8.3 2.8 11 0l43-43c2.8-2.7 2.8-7.8 0-11zM696 696c-118.8 118.7-311.2 118.7-430 0-118.7-118.8-118.7-311.2 0-430 118.8-118.7 311.2-118.7 430 0 118.7 118.8 118.7 311.2 0 430z"></path>
         </svg>
       </div>
-      <div title="Zoomer" tabindex="0" class="button search--zoom-in--js" role="button">
+      <div tooltip="Zoomer" tabindex="0" class="button search--zoom-in--js" role="button">
         <svg viewBox="64 64 896 896" focusable="false" data-icon="zoom-in" width="1em" height="1em" fill="currentColor" aria-hidden="true">
           <path d="M637 443H519V309c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v134H325c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h118v134c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V519h118c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8zm284 424L775 721c122.1-148.9 113.6-369.5-26-509-148-148.1-388.4-148.1-537 0-148.1 148.6-148.1 389 0 537 139.5 139.6 360.1 148.1 509 26l146 146c3.2 2.8 8.3 2.8 11 0l43-43c2.8-2.7 2.8-7.8 0-11zM696 696c-118.8 118.7-311.2 118.7-430 0-118.7-118.8-118.7-311.2 0-430 118.8-118.7 311.2-118.7 430 0 118.7 118.8 118.7 311.2 0 430z"></path>
         </svg>
@@ -88,12 +88,12 @@ const controllerBarHtml = /* html */ `
 
       <div tabindex="-1" class="separator display-none document-external-field-separator"></div>
 
-      <div title="Ouvrir le lien sur internet" tabindex="0" class="button display-none search--web-link--js" role="button">
+      <div tooltip="Ouvrir le lien sur internet" tabindex="0" class="button display-none search--web-link--js" role="button">
         <svg viewBox="64 64 896 896" focusable="false" data-icon="link" width="1em" height="1em" fill="currentColor" aria-hidden="true">
           <path d="M574 665.4a8.03 8.03 0 00-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 00-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 000 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 000 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 00-11.3 0L372.3 598.7a8.03 8.03 0 000 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z"></path>
         </svg>
       </div>
-      <div title="Télécharger le fichier pdf" tabindex="0" class="button display-none search--pdf-download--js" role="button">
+      <div tooltip="Télécharger le fichier pdf" tabindex="0" class="button display-none search--pdf-download--js" role="button">
         <svg viewBox="64 64 896 896" focusable="false" data-icon="file-pdf" width="1em" height="1em" fill="currentColor" aria-hidden="true">
           <path d="M531.3 574.4l.3-1.4c5.8-23.9 13.1-53.7 7.4-80.7-3.8-21.3-19.5-29.6-32.9-30.2-15.8-.7-29.9 8.3-33.4 21.4-6.6 24-.7 56.8 10.1 98.6-13.6 32.4-35.3 79.5-51.2 107.5-29.6 15.3-69.3 38.9-75.2 68.7-1.2 5.5.2 12.5 3.5 18.8 3.7 7 9.6 12.4 16.5 15 3 1.1 6.6 2 10.8 2 17.6 0 46.1-14.2 84.1-79.4 5.8-1.9 11.8-3.9 17.6-5.9 27.2-9.2 55.4-18.8 80.9-23.1 28.2 15.1 60.3 24.8 82.1 24.8 21.6 0 30.1-12.8 33.3-20.5 5.6-13.5 2.9-30.5-6.2-39.6-13.2-13-45.3-16.4-95.3-10.2-24.6-15-40.7-35.4-52.4-65.8zM421.6 726.3c-13.9 20.2-24.4 30.3-30.1 34.7 6.7-12.3 19.8-25.3 30.1-34.7zm87.6-235.5c5.2 8.9 4.5 35.8.5 49.4-4.9-19.9-5.6-48.1-2.7-51.4.8.1 1.5.7 2.2 2zm-1.6 120.5c10.7 18.5 24.2 34.4 39.1 46.2-21.6 4.9-41.3 13-58.9 20.2-4.2 1.7-8.3 3.4-12.3 5 13.3-24.1 24.4-51.4 32.1-71.4zm155.6 65.5c.1.2.2.5-.4.9h-.2l-.2.3c-.8.5-9 5.3-44.3-8.6 40.6-1.9 45 7.3 45.1 7.4zm191.4-388.2L639.4 73.4c-6-6-14.1-9.4-22.6-9.4H192c-17.7 0-32 14.3-32 32v832c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V311.3c0-8.5-3.4-16.7-9.4-22.7zM790.2 326H602V137.8L790.2 326zm1.8 562H232V136h302v216a42 42 0 0042 42h216v494z"></path>
         </svg>
@@ -101,12 +101,12 @@ const controllerBarHtml = /* html */ `
 
       <div tabindex="-1" class="separator display-none document-drive-field-separator"></div>
 
-      <div title="Autres Traductions" tabindex="0" class="button display-none search--other-traduction--js" role="button">
+      <div tooltip="Autres Traductions" tabindex="0" class="button display-none search--other-traduction--js" role="button">
         <svg viewBox="64 64 896 896" focusable="false" data-icon="translation" width="1em" height="1em" fill="currentColor" aria-hidden="true">
           <path d="M140 188h584v164h76V144c0-17.7-14.3-32-32-32H96c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h544v-76H140V188z"></path><path d="M414.3 256h-60.6c-3.4 0-6.4 2.2-7.6 5.4L219 629.4c-.3.8-.4 1.7-.4 2.6 0 4.4 3.6 8 8 8h55.1c3.4 0 6.4-2.2 7.6-5.4L322 540h196.2L422 261.4a8.42 8.42 0 00-7.7-5.4zm12.4 228h-85.5L384 360.2 426.7 484zM936 528H800v-93c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v93H592c-13.3 0-24 10.7-24 24v176c0 13.3 10.7 24 24 24h136v152c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V752h136c13.3 0 24-10.7 24-24V552c0-13.3-10.7-24-24-24zM728 680h-88v-80h88v80zm160 0h-88v-80h88v80z"></path>
         </svg>
       </div>
-      <div title="Écouter le document en audio" tabindex="0" class="button display-none search--audio-document--js" role="button">
+      <div tooltip="Écouter le document en audio" tabindex="0" class="button display-none search--audio-document--js" role="button">
         ${playIcon}
       </div>
 
@@ -115,13 +115,22 @@ const controllerBarHtml = /* html */ `
 `;
 
 const controllerBarCss = /* css */ `
+  .clearfix:before,
+  .clearfix:after {
+      content: ".";
+      display: block;
+      height: 0;
+      overflow: hidden;
+  }
+  .clearfix:after { clear: both; }
+  .clearfix { zoom: 1; }
+
   .controller--bar {
     height: 40px;
     position: absolute;
     left: 0;
     right: 0;
     bottom: 0;
-    overflow: hidden;
     z-index: 1;
     display: flex;
     justify-content: center;
@@ -132,10 +141,10 @@ const controllerBarCss = /* css */ `
     justify-content: center;
     align-items: center;
     color: #000;
-    overflow: hidden;
     width: 90%;
     max-width: 759px;
     height: 100%;
+    z-index: 1;
     background-color: #ebe9e9;
     border-radius: 2px;
     border: 1px solid #b6b6b6;
@@ -166,6 +175,7 @@ const controllerBarCss = /* css */ `
     justify-content: center;
     min-width: 33px;
     height: 70%;
+    z-index: 10000;
     flex: initial;
     margin-left: 5px;
     margin-right: 5px;
@@ -175,6 +185,158 @@ const controllerBarCss = /* css */ `
   .button svg {
     width: 100%;
     height: 100%;
+  }
+
+    /* START TOOLTIP STYLES */
+  [tooltip] {
+    position: relative; /* opinion 1 */
+  }
+
+  /* Applies to all tooltips */
+  [tooltip]::before,
+  [tooltip]::after {
+    text-transform: none; /* opinion 2 */
+    font-size: .9em; /* opinion 3 */
+    line-height: 1;
+    user-select: none;
+    pointer-events: none;
+    position: absolute;
+    display: none;
+    opacity: 0;
+  }
+  [tooltip]::before {
+    content: '';
+    border: 5px solid transparent; /* opinion 4 */
+    z-index: 1001; /* absurdity 1 */
+  }
+  [tooltip]::after {
+    content: attr(tooltip); /* magic! */
+
+    /* most of the rest of this is opinion */
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+
+    /*
+      Let the content set the size of the tooltips
+      but this will also keep them from being obnoxious
+      */
+    min-width: 3em;
+    max-width: 21em;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding: 1ch 1.5ch;
+    border-radius: .3ch;
+    box-shadow: 0 1em 2em -.5em rgba(0, 0, 0, 0.35);
+    background: #333;
+    color: #fff;
+    z-index: 1000; /* absurdity 2 */
+  }
+
+  /* Make the tooltips respond to hover */
+  [tooltip]:hover::before,
+  [tooltip]:hover::after {
+    display: block;
+  }
+
+  /* don't show empty tooltips */
+  [tooltip='']::before,
+  [tooltip='']::after {
+    display: none !important;
+  }
+
+  /* FLOW: UP */
+  [tooltip]:not([flow])::before,
+  [tooltip][flow^="up"]::before {
+    bottom: 100%;
+    border-bottom-width: 0;
+    border-top-color: #333;
+  }
+  [tooltip]:not([flow])::after,
+  [tooltip][flow^="up"]::after {
+    bottom: calc(100% + 5px);
+  }
+  [tooltip]:not([flow])::before,
+  [tooltip]:not([flow])::after,
+  [tooltip][flow^="up"]::before,
+  [tooltip][flow^="up"]::after {
+    left: 50%;
+    transform: translate(-50%, -.5em);
+  }
+
+  /* FLOW: DOWN */
+  [tooltip][flow^="down"]::before {
+    top: 100%;
+    border-top-width: 0;
+    border-bottom-color: #333;
+  }
+  [tooltip][flow^="down"]::after {
+    top: calc(100% + 5px);
+  }
+  [tooltip][flow^="down"]::before,
+  [tooltip][flow^="down"]::after {
+    left: 50%;
+    transform: translate(-50%, .5em);
+  }
+
+  /* FLOW: LEFT */
+  [tooltip][flow^="left"]::before {
+    top: 50%;
+    border-right-width: 0;
+    border-left-color: #333;
+    left: calc(0em - 5px);
+    transform: translate(-.5em, -50%);
+  }
+  [tooltip][flow^="left"]::after {
+    top: 50%;
+    right: calc(100% + 5px);
+    transform: translate(-.5em, -50%);
+  }
+
+  /* FLOW: RIGHT */
+  [tooltip][flow^="right"]::before {
+    top: 50%;
+    border-left-width: 0;
+    border-right-color: #333;
+    right: calc(0em - 5px);
+    transform: translate(.5em, -50%);
+  }
+  [tooltip][flow^="right"]::after {
+    top: 50%;
+    left: calc(100% + 5px);
+    transform: translate(.5em, -50%);
+  }
+
+  /* KEYFRAMES */
+  @keyframes tooltips-vert {
+    to {
+      opacity: .9;
+      transform: translate(-50%, 0);
+    }
+  }
+
+  @keyframes tooltips-horz {
+    to {
+      opacity: .9;
+      transform: translate(0, -50%);
+    }
+  }
+
+  /* FX All The Things */
+  [tooltip]:not([flow]):hover::before,
+  [tooltip]:not([flow]):hover::after,
+  [tooltip][flow^="up"]:hover::before,
+  [tooltip][flow^="up"]:hover::after,
+  [tooltip][flow^="down"]:hover::before,
+  [tooltip][flow^="down"]:hover::after {
+    animation: tooltips-vert 300ms ease-out forwards;
+  }
+
+  [tooltip][flow^="left"]:hover::before,
+  [tooltip][flow^="left"]:hover::after,
+  [tooltip][flow^="right"]:hover::before,
+  [tooltip][flow^="right"]:hover::after {
+    animation: tooltips-horz 300ms ease-out forwards;
   }
 `;
 
