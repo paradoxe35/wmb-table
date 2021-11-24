65bab54878e8aae327aa6ad0beacb3e034eb1595
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2021-11-24 17:59:31 +0200

    filter by testament when search on bible section

--- src/main/message-control/handlers/bible_search.ts
+++ src/main/message-control/handlers/bible_search.ts
@@ -1,23 +1,42 @@
 import { strNormalizeNoLower } from '@modules/documents/peform-search';
-import { BibleBook, BibleSearchResult, SearchMatchersValue } from '@localtypes/index';
+import {
+  BibleBook,
+  BibleSearchResult,
+  SearchMatchersValue,
+} from '@localtypes/index';
 import { performSearch } from '@root/utils/functions';
 import db, { queryDb } from '@main/db/db';
 
 let searchResults: BibleBook[] = [];
 let lastSearch: string;
 let pageNumber: number = 1;
+let lastTestament: string | undefined = undefined;
 const itemsPerPage = 10;
 
-export default async (_: any, text: string, ipageNumber?: number) => {
-  const verses = await queryDb.find<BibleBook>(db.bible);
+export default async (
+  _: any,
+  text: string,
+  ipageNumber?: number,
+  testament?: string
+) => {
+  console.log(text, ipageNumber, testament);
 
-  if (searchResults.length && lastSearch === text) {
+  if (
+    searchResults.length &&
+    lastSearch === text &&
+    lastTestament === testament
+  ) {
     pageNumber = ipageNumber || pageNumber;
   } else {
+    const verses = await queryDb.find<BibleBook>(db.bible);
+
+    lastTestament = testament;
+
     pageNumber = 1;
     lastSearch = text;
 
     searchResults = (verses
+      .filter((t) => (testament ? t.testament === testament : true))
       .map((doc: BibleBook, i) => {
         const datas = performSearch<SearchMatchersValue>(
           text,
