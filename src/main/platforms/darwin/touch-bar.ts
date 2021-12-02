5e43eecb37c4beb098f157e0571ff39c67d0e3a9
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2021-12-02 12:24:17 +0200

    touchBar callable fun to return a new instance

--- src/main/platforms/darwin/touch-bar.ts
+++ src/main/platforms/darwin/touch-bar.ts
@@ -6,11 +6,13 @@ import { IPC_EVENTS } from '@root/utils/ipc-events';
 const { TouchBar, nativeImage } = require('electron');
 const { TouchBarButton, TouchBarSpacer, TouchBarLabel } = TouchBar;
 
+
 const BG_COLOR = '#1a9aef';
 const menuView = (v: ViewMenuValue) => v;
 
 const documentViewedTitle = new TouchBarLabel({});
 
+// --------------  Option icon as touch bar button
 const optionViewerMenu = new TouchBarButton({
   icon: nativeImage
     .createFromPath(getAssetPath('img/ProfileOutlined.png'))
@@ -26,6 +28,7 @@ const optionViewerMenu = new TouchBarButton({
   },
 });
 
+//  ----------- Option document viewer as touch bar button
 const documentViewerMenu = new TouchBarButton({
   icon: nativeImage
     .createFromPath(getAssetPath('img/FundViewOutlined.png'))
@@ -41,34 +44,37 @@ const documentViewerMenu = new TouchBarButton({
   },
 });
 
-mainMessageTransport(
-  IPC_EVENTS.active_document_view,
-  (_: any, document: string) => {
-    documentViewedTitle.label = document;
-  }
-);
+// listen for change active document or title document
+const onActiveDocumentOnView = (_: any, document: string) => {
+  documentViewedTitle.label = document;
+};
 
-mainMessageTransport(
-  IPC_EVENTS.has_change_viewer_menu,
-  (_: any, view: ViewMenuValue) => {
-    if (view === 'document') {
-      documentViewerMenu.backgroundColor = BG_COLOR;
-      //@ts-ignore
-      optionViewerMenu.backgroundColor = null;
-    }
-    if (view === 'options') {
-      //@ts-ignore
-      documentViewerMenu.backgroundColor = null;
-      optionViewerMenu.backgroundColor = BG_COLOR;
-    }
+// listen for viewer menu changed
+const onChangeMenuViewer = (_: any, view: ViewMenuValue) => {
+  if (view === 'document') {
+    documentViewerMenu.backgroundColor = BG_COLOR;
+    //@ts-ignore
+    optionViewerMenu.backgroundColor = null;
   }
-);
+  if (view === 'options') {
+    //@ts-ignore
+    documentViewerMenu.backgroundColor = null;
+    optionViewerMenu.backgroundColor = BG_COLOR;
+  }
+};
 
-export const touchBar = new TouchBar({
-  items: [
-    optionViewerMenu,
-    documentViewerMenu,
-    new TouchBarSpacer({ size: 'large' }),
-    documentViewedTitle,
-  ],
-});
+// init listeners when electron app is ready
+mainMessageTransport(IPC_EVENTS.active_document_view, onActiveDocumentOnView);
+
+mainMessageTransport(IPC_EVENTS.has_change_viewer_menu, onChangeMenuViewer);
+
+// --------------  Touch bar button initialize for darwin only --------
+export const touchBar = () =>
+  new TouchBar({
+    items: [
+      optionViewerMenu,
+      documentViewerMenu,
+      new TouchBarSpacer({ size: 'large' }),
+      documentViewedTitle,
+    ],
+  });
