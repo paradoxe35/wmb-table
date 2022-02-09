874c6efa646e622cc8c3f8404d7e57c17d8a7c2f
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2022-02-09 19:22:15 +0200

    - limit the host of class methods - add comments to methods

--- src/main/features/backup/handler/prepare-restore.ts
+++ src/main/features/backup/handler/prepare-restore.ts
@@ -10,6 +10,10 @@ import fs from 'fs';
 import { promisify } from 'util';
 
 export class PrepareRestore {
+  /**
+   * Top level method to start restore preparation
+   * @returns
+   */
   static async handle() {
     commitRestoreProgress('prepare', 0, 0);
 
@@ -48,7 +52,13 @@ export class PrepareRestore {
     });
   }
 
-  static pendingDatastore(filename: string) {
+  /**
+   * Get Pending Datastore db instance by passing db filename
+   *
+   * @param filename
+   * @returns
+   */
+  private static pendingDatastore(filename: string) {
     const db = new Datastore<PendingDatastore>({
       filename: getAssetBackupPendingPath(filename),
     });
@@ -56,7 +66,13 @@ export class PrepareRestore {
     return db;
   }
 
-  static async pendingData(datastore: Datastore, filename: string) {
+  /**
+   * Store data as pending
+   *
+   * @param datastore
+   * @param filename
+   */
+  private static async pendingData(datastore: Datastore, filename: string) {
     const ids = await queryDb.find<{ _id: string }>(datastore, {}, { _id: 1 });
 
     const pendingDb = this.pendingDatastore(filename);
@@ -71,13 +87,23 @@ export class PrepareRestore {
     }
   }
 
-  static async pendingDataCustomDocument(datastore: Datastore) {
+  /**
+   * Store custom document as pending
+   *
+   * @param datastore
+   */
+  private static async pendingDataCustomDocument(datastore: Datastore) {
     const docs = await queryDb.find<CustomDocument>(datastore);
 
     await this.pendingDocumentsTitle(docs);
   }
 
-  static async pendingDocumentsTitle(docs: CustomDocument[]) {
+  /**
+   * Feaching titles in titels's db from custom document db and store them as pending
+   *
+   * @param docs
+   */
+  private static async pendingDocumentsTitle(docs: CustomDocument[]) {
     const documentsTitleDb = this.pendingDatastore(
       getDatastoreFileName(db.documentsTitle)!
     );
