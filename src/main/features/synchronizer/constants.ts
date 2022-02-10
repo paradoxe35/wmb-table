0e82c0e35bbb2172382157ed87cf5b6bd6b2f8c7
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2022-02-11 01:28:11 +0200

    firestore instance constant

--- src/main/features/synchronizer/constants.ts
+++ src/main/features/synchronizer/constants.ts
@@ -1,10 +1,19 @@
+import * as admin from 'firebase-admin';
+
 export const SYNCHRONIZER_UPLOADING: { value: boolean } = {
   value: false,
+};
+
+export const FIRESTORE_INSTANCE: { value: admin.firestore.Firestore | null } = {
+  value: null,
 };
 
 export const SYNCHRONIZER_DOWNLOADING: { value: boolean } = {
   value: false,
 };
+
+export const setFirestoreInstance = (value: admin.firestore.Firestore) =>
+  (FIRESTORE_INSTANCE.value = value);
 
 export const setSynchronizerUploader = (value: boolean) =>
   (SYNCHRONIZER_UPLOADING.value = value);
