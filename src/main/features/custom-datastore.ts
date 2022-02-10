9c522eccefe2bea0b499f1add766a4e70c8df59b
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2022-02-10 18:43:49 +0200

    CustomDatastore feature

---
+++ src/main/features/custom-datastore.ts
@@ -0,0 +1,57 @@
+import path from 'path';
+import Nedb from '@seald-io/nedb';
+import { DB_EXTENSION } from '@root/utils/constants';
+
+export default class CustomDatastore<T> {
+  protected datastore: Nedb<T>;
+
+  constructor(dbpath: string, dbname: string) {
+    this.datastore = new Nedb<T>({
+      filename: path.join(dbpath, dbname + DB_EXTENSION),
+      timestampData: true,
+    });
+
+    this.datastore.loadDatabase();
+  }
+
+  public data(): Promise<T | undefined> {
+    return new Promise<T | undefined>((resolve) => {
+      this.datastore.findOne({}, {}, (err, document) => {
+        if (err) {
+          return resolve(undefined);
+        }
+        resolve(document);
+      });
+    });
+  }
+
+  public datas(): Promise<T[]> {
+    return new Promise<T[]>((resolve) => {
+      this.datastore.find({}, {}, (err, documents) => {
+        if (err) {
+          return resolve([]);
+        }
+        resolve(documents);
+      });
+    });
+  }
+
+  public clear_datastore() {
+    return new Promise<number>((resolve) =>
+      this.datastore.remove({}, { multi: true }, (_, n) => {
+        resolve(n);
+      })
+    );
+  }
+
+  public create(fresh: T) {
+    return new Promise<T>((resolve) => {
+      this.datastore.insert(fresh, (err, created) => {
+        if (err) {
+          return resolve({} as T);
+        }
+        return resolve(created);
+      });
+    });
+  }
+}
