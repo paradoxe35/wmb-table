09e3e199c8e486036cb75a73e71699c77e68b594
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2022-02-12 04:09:59 +0200

    UniqueLoopProcess: Uniquify a loop process

--- src/main/features/synchronizer/utils.ts
+++ src/main/features/synchronizer/utils.ts
@@ -25,3 +25,52 @@ export function initializeFireorm() {
 
   return firestore;
 }
+
+/**
+ * Uniquify a loop process
+ */
+export class UniqueLoopProcess {
+  private requesting: boolean = false;
+  private timer: NodeJS.Timer | undefined;
+  private closed: boolean = false;
+
+  constructor(private INTERVAL: number = 2000) {
+    this.loop = this.loop.bind(this);
+  }
+
+  // callback function in params
+  public loop(callback: () => Promise<void>) {
+    this._callback = callback;
+    this.timer = setInterval(this.unique_loop, this.INTERVAL);
+  }
+
+  private clearInterval() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = undefined;
+    }
+  }
+
+  private async _callback() {}
+
+  private async unique_loop() {
+    if (this.requesting) {
+      this.clearInterval();
+      return;
+    }
+    this.requesting = true;
+
+    await this._callback();
+
+    this.requesting = false;
+
+    if (!this.timer && !this.closed) {
+      this.timer = setInterval(this.unique_loop, this.INTERVAL);
+    }
+  }
+
+  public stop() {
+    this.closed = true;
+    this.clearInterval();
+  }
+}
